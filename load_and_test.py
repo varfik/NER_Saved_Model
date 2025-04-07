c2e88813951ee80a8195a720fd732625b0d8fab0
varfik <arinavarf@yandex.ru>
2025-04-08 00:18:52 +0300

    still fixing ex

--- load_and_test.py
+++ load_and_test.py
@@ -1,14 +1,23 @@
 from NEREL import NERRelationModel
 from NEREL import predict
 from transformers import AutoTokenizer
+
 if __name__ == "__main__":
     # Для загрузки модели
     loaded_model = NERRelationModel.from_pretrained("saved_model")
     loaded_tokenizer = AutoTokenizer.from_pretrained("saved_model")
     
+    # Текст для анализа
+    text = "По улице шел красивый человек, его имя было Мефодий. И был он счастлив. Работал этот чувак в яндексе, разработчиком. Или директором. Он пока не определился!"
+    
     # Использование модели
-    result = predict("По улице шел красивый человек, его имя было Мефодий. И был он счастлив. Работал этот чувак в яндексе, разработчиком. Или директором. Он пока не определился!", loaded_model, loaded_tokenizer)
+    result = predict(text, loaded_model, loaded_tokenizer)
+    
+    # Вывод результатов
     print("\nEntities:")
-    print(f"{result['type']}: {result['text']}")
+    for entity in result['entities']:
+        print(f"{entity['type']}: {entity['text']}")
+    
     print("\nRelations:")
-    print(f"{result['type']}: {result['arg1']['text']} -> {result['arg2']['text']} (conf: {result['confidence']:.2f})")
+    for relation in result['relations']:
+        print(f"{relation['type']}: {relation['arg1']['text']} -> {relation['arg2']['text']} (conf: {relation['confidence']:.2f})")
