bcf47cb62e1561c3f8360c4594a8633548567a6c
varfik <arinavarf@yandex.ru>
2025-04-08 00:09:06 +0300

    added output

--- load_and_test.py
+++ load_and_test.py
@@ -8,3 +8,9 @@ if __name__ == "__main__":
     
     # Использование модели
     result = predict("По улице шел красивый человек, его имя было Мефодий. И был он счастлив. Работал этот чувак в яндексе, разработчиком. Или директором. Он пока не определился!", loaded_model, loaded_tokenizer)
+    print("\nEntities:")
+        for e in result['entities']:
+            print(f"{e['type']}: {e['text']}")
+        print("\nRelations:")
+        for r in result['relations']:
+            print(f"{r['type']}: {r['arg1']['text']} -> {r['arg2']['text']} (conf: {r['confidence']:.2f})")
