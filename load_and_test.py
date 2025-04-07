4abd756356579c05100dedce2ebac1a0740a84ae
varfik <arinavarf@yandex.ru>
2025-04-08 00:15:14 +0300

    one example for test, not list

--- load_and_test.py
+++ load_and_test.py
@@ -9,8 +9,6 @@ if __name__ == "__main__":
     # Использование модели
     result = predict("По улице шел красивый человек, его имя было Мефодий. И был он счастлив. Работал этот чувак в яндексе, разработчиком. Или директором. Он пока не определился!", loaded_model, loaded_tokenizer)
     print("\nEntities:")
-        for e in result['entities']:
-            print(f"{e['type']}: {e['text']}")
-        print("\nRelations:")
-        for r in result['relations']:
-            print(f"{r['type']}: {r['arg1']['text']} -> {r['arg2']['text']} (conf: {r['confidence']:.2f})")
+    print(f"{result['type']}: {result['text']}")
+    print("\nRelations:")
+    print(f"{result['type']}: {result['arg1']['text']} -> {result['arg2']['text']} (conf: {result['confidence']:.2f})")
