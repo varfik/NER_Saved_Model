4706e84cde16e56581f5e5983f9b879d92c0c84f
varfik <arinavarf@yandex.ru>
2025-04-08 00:03:43 +0300

    renamed to NEREL and upgrade test file

--- load_and_test.py
+++ load_and_test.py
@@ -1,1 +1,10 @@
 import NERRelationModel
+from NEREL import predict
+from transformers import AutoTokenizer
+if __name__ == "__main__":
+    # Для загрузки модели
+    loaded_model = NERRelationModel.from_pretrained("saved_model")
+    loaded_tokenizer = AutoTokenizer.from_pretrained("saved_model")
+    
+    # Использование модели
+    result = predict("По улице шел красивый человек, его имя было Мефодий. И был он счастлив. Работал этот чувак в яндексе, разработчиком. Или директором. Он пока не определился!", loaded_model, loaded_tokenizer)
