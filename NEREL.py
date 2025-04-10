3ec4e4dc899bba663954b08c2467b4cb820f90a2
varfik <arinavarf@yandex.ru>
2025-04-11 02:18:25 +0300

    40 epoch

--- NEREL.py
+++ NEREL.py
@@ -652,7 +652,7 @@ def train_model():
     # Training loop
     best_ner_f1 = 0
     # Цикл обучения
-    for epoch in range(20):
+    for epoch in range(40):
         model.train()
         epoch_loss = 0
         ner_correct = ner_total = 0
